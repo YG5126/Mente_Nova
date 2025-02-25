8835a4e9d31cdfca214a245d02e929d8db53d5cc
YG5126 <yarikgnes@yandex.ru>
2025-02-25 09:49:14 +0300

    first commit

---
+++ src/main/java/mente/nova/mente_nova/MenteNovaApplication.java
@@ -0,0 +1,13 @@
+package mente.nova.mente_nova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenteNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenteNovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mente/nova/mente_nova/MenteNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package mente.nova.mente_nova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenteNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
